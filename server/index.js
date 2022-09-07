b66ef1a074419ce7a5f627bd60b56e902d82848b
Dan Thackray <djthackray@gmail.com>
2022-09-07 10:51:29 +0100

    Connect to MongoDB Atlas
    Signed-off-by: Dan Thackray <djthackray@gmail.com>

--- server/index.js
+++ server/index.js
@@ -7,3 +7,13 @@ const app = express()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+app.use(cors())
+
+// https://mongodb.com/cloud/atlas
+const CONNECTION_URL = "mongodb+srv://memories:memories123@cluster0.9lim2q7.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message))
